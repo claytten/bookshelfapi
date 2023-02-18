fa4c064173965f1c5be3b65234cacffbb2f61712
aji <wahyuajisulaiman@gmail.com>
2023-02-19 06:53:36 +0700

    delete prefix

--- internal/routes.js
+++ internal/routes.js
@@ -6,41 +6,39 @@ const {
   deleteBookByIdHandler,
 } = require('./handlers');
 
-const prefixApi = '/api/v1';
-
 const publicRoutes = [
   // creating book
   {
     method: 'POST',
-    path: `${prefixApi}/books/create`,
+    path: '/books',
     handler: addBookHandler,
   },
 
   // get book by id
   {
     method: 'GET',
-    path: `${prefixApi}/books/{bookId}`,
+    path: '/books/{bookId}',
     handler: getBookByIdHandler,
   },
 
   // get all books
   {
     method: 'GET',
-    path: `${prefixApi}/books`,
+    path: '/books',
     handler: getAllBooksHandler,
   },
 
   // edit book by id
   {
     method: 'PUT',
-    path: `${prefixApi}/books/{bookId}`,
+    path: '/books/{bookId}',
     handler: editBookByIdHandler,
   },
 
   // delete book by id
   {
     method: 'DELETE',
-    path: `${prefixApi}/books/{bookId}`,
+    path: '/books/{bookId}',
     handler: deleteBookByIdHandler,
   },
 ];
